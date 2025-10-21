ad42638e353ee47f9e83e72d90e2df3908198326
The B <bekirhan.kurt@outlook.com>
2025-10-21 12:17:03 +0300

    Add validation pipeline with FluentValidation and MediatR
    Introduced a robust validation pipeline leveraging FluentValidation
    and MediatR. Updated `Core.Application.csproj` to include required
    package references and project dependencies.
    
    Enhanced the `ExceptionHandler` abstract class to support
    `ValidationException` handling. Updated `HttpExceptionHandler` to
    return structured `ValidationProblemDetails` for validation errors
    and handle generic exceptions with appropriate HTTP status codes.
    
    Added `RequestValidationBehavior` to validate requests in the
    MediatR pipeline. Validation errors are aggregated and thrown as
    `ValidationException`.
    
    Created `ValidationProblemDetails` to represent validation errors
    in HTTP responses. Introduced `ValidationException` and
    `ValidationExceptionModel` to encapsulate and format validation
    error details.
    
    These changes improve the application's ability to validate
    requests and communicate validation errors in a structured and
    standardized manner.

--- Core.CrossCuttingConcerns/Exceptions/Handlers/ExceptionHandler.cs
+++ Core.CrossCuttingConcerns/Exceptions/Handlers/ExceptionHandler.cs
@@ -9,12 +9,16 @@ public abstract class ExceptionHandler
         {
             BusinessException businessException =>
                 HandleException(businessException),
+            ValidationException validationException =>
+                HandleException(validationException),
             _ => HandleException(exception)
 
         };
 
 
     protected abstract Task HandleException(BusinessException businessException);
+
+    protected abstract Task HandleException(ValidationException validationException);
 
     protected abstract Task HandleException(Exception exception);
 
---
+++ Core.CrossCuttingConcerns/Exceptions/Types/ValidationException.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Types;
+
+public class ValidationException : Exception
+{
+    public IEnumerable<ValidationExceptionModel> Errors { get; }
+
+    public ValidationException()
+        : base()
+    {
+        Errors = [];
+    }
+
+    public ValidationException(string message)
+        : base(message)
+    {
+        Errors = [];
+    }
+
+    public ValidationException(string message, Exception innerException) : base(message, innerException)
+    {
+        Errors = [];
+    }
+
+    public ValidationException(IEnumerable<ValidationExceptionModel> errors) : base(BuildErrorMessages(errors))
+    {
+        
+    }
+
+    private static string BuildErrorMessages(IEnumerable<ValidationExceptionModel> errors)
+    {
+        var arr = errors.Select(x =>
+            $"{Environment.NewLine} -- {x.Property} : {string.Join(Environment.NewLine, values: x.Errors)}"
+        );
+        return $"Validation Failed: {string.Join(string.Empty, arr)}";
+    }
+
+}
+
+public class ValidationExceptionModel
+{
+    public string? Property { get; set; }
+    public IEnumerable<string> Errors { get; set; }
+
+}
